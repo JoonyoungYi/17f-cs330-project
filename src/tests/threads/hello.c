da569c3e38d9f6112ca99caaa8c99359505c0da4
Eunseok Jeong <dakerhope@kaist.ac.kr>
2017-09-22 18:40:22 +0900

    init 2

---
+++ src/tests/threads/hello.c
@@ -0,0 +1,14 @@
+
+
+//  make the print hello world
+
+#include <stdio.h>
+//#include "tests/threads/tests.h"
+//#include "threads/init.h"
+
+
+void test_hello (void)
+{
+  printf("hello, world!\n");
+}
+
