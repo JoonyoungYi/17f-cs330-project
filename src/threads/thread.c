fd7d8ae092c08efd499a22366ca8583d1ffd1809
Joonyoung Yi <joonyoung@bablabs.com>
2017-09-23 15:08:47 +0900

    아마도 되어야 함..

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -572,34 +572,38 @@ next_thread_to_run (void)
   if (list_empty (&ready_list))
     return idle_thread;
 
-  // struct list_elem *e;
-  // struct thread *t_maxp = NULL; // thread with max priority
-  // struct list_elem *e_maxp = NULL; // list_elem with max priority
-  // int maxp = PRI_MIN-1; // init with exceptional priority
-  // for (e = list_begin (&ready_list); e != list_end (&ready_list);
-  //      e = list_next (e))
-  //   {
-  //     struct thread *t = list_entry (e, struct thread, elem);
-  //     if (maxp <= PRI_MIN-1)
-  //       {
-  //         t_maxp = t;
-  //         e_maxp = e;
-  //         maxp = t_maxp->priority;
-  //       }
-  //     else
-  //       {
-  //         // if (maxp > t_maxp->priority) {
-  //         //   t_maxp = t;
-  //         // }
-  //       }
-  //   }
+
+
   // list_remove(&e_maxp);
   // return &t_maxp;
   //
   // return list_entry (list_pop_front (&ready_list), struct thread, elem);
-  struct list_elem *e = list_begin(&ready_list);
-  list_remove(e);
-  return list_entry (e, struct thread, elem);
+
+  struct list_elem *e;
+  struct thread *t_maxp = NULL; // thread with max priority
+  struct list_elem *e_maxp = NULL; // list_elem with max priority
+  int maxp = PRI_MIN-1; // init with exceptional priority
+
+  for (e = list_begin (&ready_list); e != list_end (&ready_list);
+       e = list_next (e))
+    {
+      struct thread *t = list_entry (e, struct thread, elem);
+      if (maxp <= PRI_MIN-1)
+        {
+          t_maxp = t;
+          e_maxp = e;
+          maxp = t_maxp->priority;
+        }
+      else
+        {
+          // if (maxp > t_maxp->priority) {
+          //   t_maxp = t;
+          // }
+        }
+    }
+
+  list_remove(e_maxp);
+  return list_entry (e_maxp, struct thread, elem);
 }
 
 /* Completes a thread switch by activating the new thread's page
