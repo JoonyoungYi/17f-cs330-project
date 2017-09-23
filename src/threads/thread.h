186250912cbfd22f7f0ba8cab3c94fdf090dc772
Joonyoung Yi <joonyoung@bablabs.com>
2017-09-23 18:10:00 +0900

    header 수정

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -149,8 +149,8 @@ void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
 
-static bool list_priority_less_func (const struct list_elem *a,
-                                     const struct list_elem *b,
-                                     void *aux);
+bool list_priority_less_func (const struct list_elem *a,
+                              const struct list_elem *b,
+                              void *aux);
 
 #endif /* threads/thread.h */
