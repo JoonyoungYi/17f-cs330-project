95d6c8e725ad2e62af1feb81d287eb031295597f
Joonyoung Yi <joonyoung@bablabs.com>
2017-09-24 22:06:21 +0900

    log

--- src/tests/threads/priority-donate-nest.c
+++ src/tests/threads/priority-donate-nest.c
@@ -53,7 +53,12 @@ test_priority_donate_nest (void)
   msg ("Low thread should have priority %d.  Actual priority: %d.",
        PRI_DEFAULT + 2, thread_get_priority ());
 
-  lock_release (&a);
+  msg ("priority=%d, target_lock=%s, donors_size=%d",
+       thread_get_priority (),
+       thread_current()->waiting_lock,
+       list_size(&thread_current()->donated_threads));
+
+  // lock_release (&a);
   // thread_yield ();
   // msg ("Medium thread should just have finished.");
   // msg ("Low thread should have priority %d.  Actual priority: %d.",
