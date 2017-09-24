06258f8fd1c07e220952cb95b651dc0d0ecbb74d
Joonyoung Yi <joonyoung@bablabs.com>
2017-09-24 18:02:33 +0900

    all comment

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -330,9 +330,9 @@ lock_release (struct lock *lock)
   ASSERT (lock != NULL);
   ASSERT (lock_held_by_current_thread (lock));
 
-  priority_return (lock);
+  // priority_return (lock);
   // remove_unrelated_threads (lock);
-  priority_refresh ();
+  // priority_refresh ();
 
   lock->holder = NULL;
   sema_up (&lock->semaphore);
