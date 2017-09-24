8474b2216b4f70ea6f36b9f2478b7c8ece394b45
Joonyoung Yi <joonyoung@bablabs.com>
2017-09-24 19:32:48 +0900

    remove unused

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -425,28 +425,6 @@ thread_yield (void)
   intr_set_level (old_level);
 }
 
-void
-priority_refresh ()
-{
-  struct thread *curr = thread_current ();
-  if (list_empty (&curr->donated_threads))
-    return;
-
-  int max_priority = PRI_MIN;
-  struct list_elem *e;
-  for (e = list_begin (&curr->donated_threads);
-       e != list_end (&curr->donated_threads);
-       e = list_next (e))
-    {
-      struct thread *t = list_entry (e, struct thread, donated_elem);
-      if (max_priority < t->priority)
-        max_priority = t->priority;
-    }
-
-  if (curr->initial_priority < max_priority)
-    curr->priority = max_priority;
-}
-
 /* Sets the current thread's priority to NEW_PRIORITY. */
 void
 thread_set_priority (int new_priority)
