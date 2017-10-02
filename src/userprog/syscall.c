538615ea0e9fd8f3fa08b8e13a6b7b1adc12355d
Joonyoung Yi <joonyoung@bablabs.com>
2017-10-02 23:04:17 +0900

    debug 106

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -37,8 +37,8 @@ check_user_ptr_validation (void *ptr)
 {
   check_ptr_validation(ptr);
   printf(">> check_user_ptr_validation: start\n");
-  printf(">> check_user_ptr_validation: ptr -> %d\n", ptr);
-  printf(">> check_user_ptr_validation: pagedir -> %d\n", thread_current()->pagedir);
+  printf(">> check_user_ptr_validation: ptr -> 0x%x\n", ptr);
+  printf(">> check_user_ptr_validation: pagedir -> 0x%x\n", thread_current()->pagedir);
   if (pagedir_get_page (thread_current()->pagedir, ptr) == NULL);
     {
       printf(">> check_user_ptr_validation: exit\n");
