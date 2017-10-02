136a4ea739c4246481ed81c50169c10e0ea97f2e
Joonyoung Yi <joonyoung@bablabs.com>
2017-10-02 21:26:02 +0900

    debug 89

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -24,7 +24,7 @@ void
 check_ptr_validation (void *ptr)
 {
   printf(">> check_ptr_validation\n");
-  printf ('>> check_ptr_validation: ptr -> 0x%x', ptr);
+  printf ('>> check_ptr_validation: ptr -> %d', ptr == NULL);
   // if (((unsigned int) ptr) <= 0x8048000 ||
   //       ((unsigned int) ptr) >= 0xc0000000)
   //   exit(-1);
