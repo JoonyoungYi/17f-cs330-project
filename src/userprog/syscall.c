e0614a175460b666d2a1d97d515d9cc56ff6356e
Joonyoung Yi <joonyoung@bablabs.com>
2017-10-06 17:47:39 +0900

    debug 45

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -94,11 +94,11 @@ syscall_handler (struct intr_frame *f)
       case SYS_WRITE:                  /* Write to a file. */
         ; // printf (">> syscall_handler: case SYS_WRITE\n");
         int fd = (int) read_argument (esp + 1);
-        printf (">> syscall_handler: case SYS_WRITE fd -> %d\n", fd);
+        // printf (">> syscall_handler: case SYS_WRITE fd -> %d\n", fd);
         void *buffer = (void*) read_argument (esp + 2);
-        printf (">> syscall_handler: case SYS_WRITE buffer\n");
+        // printf (">> syscall_handler: case SYS_WRITE buffer\n");
         unsigned length = (unsigned) read_argument (esp + 3);
-        printf (">> syscall_handler: case SYS_WRITE length -> %d\n", length);
+        // printf (">> syscall_handler: case SYS_WRITE length -> %d\n", length);
         f->eax = write (fd, buffer, length);
         break;
       case SYS_SEEK:                   /* Change position in a file. */
