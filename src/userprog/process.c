29aa8196299c46ed728edeaf41387471727aeafb
Joonyoung Yi <joonyoung@bablabs.com>
2017-10-08 15:28:16 +0900

    debug 4

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -234,7 +234,7 @@ process_add_file (struct file *f)
   tf->fd = fd;
   tf->f = f;
 
-  // struct thread *curr = thread_current ();
+  struct thread *curr = thread_current ();
   // list_push_back (&curr->thread_files, &tf->elem);
   curr->fd_max = fd;
   return fd;
