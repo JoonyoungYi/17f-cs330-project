5d4d15bb383d733834b4681e60a6ab7175a646cf
Joonyoung Yi <joonyoung@bablabs.com>
2017-10-09 14:37:37 +0900

    debug 6

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -148,7 +148,7 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-  // printf(">> page fault called!\n");
+  printf(">> page fault called!\n");
   exit (-1);
 
   // TODO: remove
